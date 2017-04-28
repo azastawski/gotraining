3cf144f3077e3f3a8416a23cf8984b989e8ffc9b
Adam Zastawski <azastawski@gmail.com>
2017-04-28 15:46:35 -0500

    played with iotas

--- 01_sample.go
+++ 01_sample.go
@@ -4,11 +4,29 @@ package main
 
 import "fmt"
 
+const p string = "death and taxes"
+const q = 42
+
+const (
+	A = iota
+	B = iota
+)
+
+const (
+	C = iota //0
+	D
+)
+
 func main() {
 	fmt.Println("Hello, world")
 	PrintVariables("Jan Michael Smith")
+	fmt.Println(A)
+	fmt.Println(B)
+	fmt.Println(C)
+	fmt.Println(D)
 }
 
+//this is exported?
 func PrintVariables(name string) {
 	var b string
 
@@ -16,4 +34,15 @@ func PrintVariables(name string) {
 
 	b = name
 	fmt.Println(b)
+
+	fmt.Println(reverse(b))
+}
+
+//this is not exported
+func reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
 }
